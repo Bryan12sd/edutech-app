be4d4366d3a230018919f076677ad5a56b7e9210
Bryan Cordero <bryanzeta17@gmail.com>
2026-05-01 20:42:01 -0500

    Refactor app structure: replace main App component with AppNavigator and update dependencies

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from '../screens/loginScreen';
+import RegisterScreen from '../screens/registerScreen';
+import HomeScreen from '../screens/homeScreen';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
